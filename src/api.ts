d7a9777a22478d6dc47b328ae689e8a301f75cfa
Dan Groleau <ddgroleau@gmail.com>
2023-01-05 20:02:17 -0500

    Add console logs to debug IOS issues

--- src/api.ts
+++ src/api.ts
@@ -5,7 +5,9 @@ export const useGetLocationsInRadius = (radius:number,latitude:number,longitude:
     const baseApiUri = "http://localhost:5000/api";
     return useQuery('GET_LOCATIONS_IN_RADIUS',async ()=> {
         const requestUri = baseApiUri + `/locations/radius/${radius}?latitude=${latitude}&longitude=${longitude}`;
+        console.log(requestUri);
         const response = await axios.get(requestUri);
+        console.log(response);
         return response.data;
     }, { enabled: false });
 };
